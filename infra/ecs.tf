b26f42f4c5d81ab24ca76ccc7d49411904d191d2
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-06-29 00:05:14 +0700

    update ecs config

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -3,13 +3,18 @@ resource "aws_ecs_cluster" "chat_app_cluster" {
 }
 
 resource "aws_ecs_service" "app" {
-  name        = "chat_app_service"
-  cluster     = aws_ecs_cluster.chat_app_cluster.id
-  launch_type = "FARGATE"
+  name            = "chat_app_service"
+  cluster         = aws_ecs_cluster.chat_app_cluster.id
+  task_definition = aws_ecs_task_definition.chat_app_task_definition.id
+  launch_type     = "FARGATE"
 }
 
-resource "aws_ecs_task_definition" "chat_app_task" {
-  family = "chat_app_tasks"
+resource "aws_ecs_task_definition" "chat_app_task_definition" {
+  family                   = "chat_app_tasks"
+  cpu                      = 256
+  memory                   = 512
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
   container_definitions = jsonencode([
     {
       name  = "chat_app"
