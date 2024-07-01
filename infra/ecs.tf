abe95a424482f2e4156f1e7c68a8256cd223339b
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-07-01 14:26:04 +0700

    Revert "remove load balancer"
    This reverts commit 573db2f32b9b9a82350902e825c0ae5488701c5a.

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -10,10 +10,18 @@ resource "aws_ecs_service" "chat_app_service" {
   force_new_deployment = true
   desired_count        = 1
   network_configuration {
-    subnets          = [aws_subnet.chat_app_subnet.id]
+    subnets          = [aws_subnet.chat_app_subnet.id, aws_subnet.chat_app_subnet_2.id]
     security_groups  = [aws_security_group.chat_app_security_group.id]
     assign_public_ip = true
   }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.chat_app_lb_target_group.id
+    container_name   = "chat_app"
+    container_port   = 8080
+  }
+
+  depends_on = [aws_lb_listener.chat_app_lb_listener]
 }
 
 resource "aws_ecs_task_definition" "chat_app_task_definition" {
---
+++ infra/load_balancer.tf
@@ -0,0 +1,23 @@
+resource "aws_lb" "chat_app_lb" {
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.chat_app_security_group.id]
+  subnets            = [aws_subnet.chat_app_subnet.id, aws_subnet.chat_app_subnet_2.id]
+}
+
+resource "aws_lb_target_group" "chat_app_lb_target_group" {
+  port        = 8080
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.chat_app_vpc.id
+  target_type = "ip"
+}
+
+resource "aws_lb_listener" "chat_app_lb_listener" {
+  load_balancer_arn = aws_lb.chat_app_lb.arn
+  port              = "8080"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.chat_app_lb_target_group.arn
+  }
+}
