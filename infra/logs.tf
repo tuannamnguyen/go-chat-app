00d431d39fb4bef2740834e149f8a338df9fec4a
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-07-02 15:06:42 +0700

    remove log stream

--- infra/logs.tf
+++ infra/logs.tf
@@ -3,8 +3,3 @@ resource "aws_cloudwatch_log_group" "chat_app_log_group" {
   name              = "/ecs/chat_app"
   retention_in_days = 30
 }
-
-resource "aws_cloudwatch_log_stream" "chat_app_log_stream" {
-  name           = "chat_app_log_stream"
-  log_group_name = aws_cloudwatch_log_group.chat_app_log_group.name
-}
