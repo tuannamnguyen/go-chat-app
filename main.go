4e3ca65a78219c51c408f215921790cf7c1c8175
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-07-02 15:24:46 +0700

    test update

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.String(http.StatusOK, "Hello, Nam!")
 	})
 	
 	e.Logger.Fatal(e.Start(":8080"))
