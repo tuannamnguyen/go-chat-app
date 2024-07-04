f5dea1648beb78f6024677b7db6ddb10b0aa9815
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-07-04 12:39:33 +0700

    update login route

--- auth_route_handler.go
+++ auth_route_handler.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"net/http"
 	"os"
 
@@ -9,22 +11,29 @@ import (
 	"golang.org/x/oauth2/google"
 )
 
+func generateStateOauthCookie() string {
+	b := make([]byte, 16)
+	rand.Read(b)
+	state := base64.URLEncoding.EncodeToString(b)
+
+	return state
+}
+
 func loginRouteHandler(c echo.Context) error {
 	conf := &oauth2.Config{
-		ClientID: os.Getenv("CLIENT_ID"),
+		ClientID:     os.Getenv("CLIENT_ID"),
 		ClientSecret: os.Getenv("CLIENT_SECRET"),
-		RedirectURL: os.Getenv("REDIRECT_URL"),
+		RedirectURL:  os.Getenv("REDIRECT_URL"),
 		Scopes: []string{
 			"https://www.googleapis.com/auth/userinfo.email",
 			"https://www.googleapis.com/auth/userinfo.profile",
 		},
 		Endpoint: google.Endpoint,
 	}
-
-	url := conf.AuthCodeURL("state")
-	return c.String(http.StatusOK, url)
+	url := conf.AuthCodeURL(generateStateOauthCookie(), oauth2.AccessTypeOffline, oauth2.ApprovalForce)
+	return c.JSON(http.StatusOK, &apiResponse{Data: url})
 }
 
 func callbackRouteHandler(c echo.Context) error {
-	return c.String(http.StatusOK, "abc")
+	return c.String(http.StatusOK, "abcd")
 }
---
+++ response_model.go
@@ -0,0 +1,5 @@
+package main
+
+type apiResponse struct {
+	Data any `json:"data"`
+}
